7ba8bd286f0e898ed0f9d986fdf90c87c50c28cf
Chen Wang <unicorn_wang@outlook.com>
2024-06-25 09:47:15 +0800

    add sample code to test the float operation
    Signed-off-by: Chen Wang <unicorn_wang@outlook.com>

--- test_main.c
+++ test_main.c
@@ -1,8 +1,32 @@
 #include <rtthread.h>
 #include <stdio.h>
 
+static float ff(float f, int i)
+{
+    return f/i;
+}
+
+static double dd(double d, float f)
+{
+    return d * f;
+}
+
 int main(void)
 {
+    int i= 100;
+    float f = 123;
+    double d = 1.1;
+
     rt_kprintf("Hello RTT test!\n");
+
+    f = ff(f, i);
+    // rt_kprintf 不支持浮点数打印
+    // 如果需要实现 rt_kprintf 打印浮点数，参考 <https://github.com/mysterywolf/rt_vsnprintf_full>
+    // 也可以简单使用 printf，但在多任务下存在安全风险
+    printf("printf float f = %f\n", f);
+
+    d = dd(d, f);
+    printf("printf double d = %f\n", d);
+
     return 0;
 }
